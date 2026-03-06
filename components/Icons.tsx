43e4202f00272a6bf4964637885201102ab216c7
selectclass <agendaselectclass@gmail.com>
2026-03-06 10:31:08 -0300

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -301,6 +301,13 @@ export const MicIcon: React.FC<{ className?: string }> = ({ className }) => (
   </svg>
 );
 
+export const BellIcon: React.FC<{ className?: string }> = ({ className }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
+    <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
+    <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+  </svg>
+);
+
 export const MailIcon: React.FC<{ className?: string }> = ({ className }) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
     <rect width="20" height="16" x="2" y="4" rx="2" />
