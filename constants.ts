8b6d544f1d0aaca06b226953e4fceed23099417b
selectclass <agendaselectclass@gmail.com>
2026-05-06 20:11:43 -0300

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,17 @@
+export const COUNTRY_CODES = [
+  { code: '+55', country: 'Brasil' },
+  { code: '+1', country: 'Estados Unidos' },
+  { code: '+44', country: 'Reino Unido' },
+  { code: '+351', country: 'Portugal' },
+  { code: '+34', country: 'Espanha' },
+  { code: '+33', country: 'França' },
+  { code: '+49', country: 'Alemanha' },
+  { code: '+39', country: 'Itália' },
+  { code: '+81', country: 'Japão' },
+  { code: '+61', country: 'Austrália' },
+  { code: '+54', country: 'Argentina' },
+  { code: '+56', country: 'Chile' },
+  { code: '+57', country: 'Colômbia' },
+  { code: '+52', country: 'México' },
+  // ... can be expanded
+];
