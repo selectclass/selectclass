7d44b61c5f54a2364d33c702d1dd577b232336c5
selectclass <agendaselectclass@gmail.com>
2026-07-07 21:31:41 -0300

    Update types.ts

--- types.ts
+++ types.ts
@@ -18,6 +18,7 @@ export enum AppView {
 export interface CotacaoCategoria {
   id: string;
   name: string;
+  order?: number;
 }
 
 export interface CotacaoItem {
@@ -25,6 +26,7 @@ export interface CotacaoItem {
   categoryId: string;
   description: string;
   value: number;
+  included?: boolean;
 }
 
 export interface Cotacao {
