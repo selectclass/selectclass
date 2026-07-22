89788176ccd763ce060ac8b5104fd375ca65ece8
selectclass <agendaselectclass@gmail.com>
2026-07-22 08:36:48 -0300

    Delete fix.js

--- fix.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-let code = fs.readFileSync('components/CotacoesScreen.tsx', 'utf8');
-code = code.replace(/lectureModels,\s*targetCotacaoId,\s*onClearTargetCotacao=\{lectureModels,\s*targetCotacaoId,\s*onClearTargetCotacao\}/g, 'lectureModels={lectureModels}');
-fs.writeFileSync('components/CotacoesScreen.tsx', code);
